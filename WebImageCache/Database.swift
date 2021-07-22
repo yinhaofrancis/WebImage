f154668c77338ba20d598d864a8fb16febde7981
Francis <test@git.com>
2021-07-23 02:00:31 +0800

    db

--- WebImageCache/Database.swift
+++ WebImageCache/Database.swift
@@ -13,44 +13,154 @@ public class Database{
     public let queue:DispatchQueue
     public let url:URL
     public var sqlite:OpaquePointer?
+    public var functions:Array<ScalarFunction> = Array()
     public init(group:DispatchGroup,queue:DispatchQueue,name:String) throws{
         self.group = group
         self.queue = queue
         let url = try Database.checkCacheDir().appendingPathComponent(name)
         self.url = url
         sqlite3_open_v2(url.path, &self.sqlite, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nil)
+        if(self.sqlite == nil){
+            throw NSError(domain: "create sqlite3 fail", code: 0, userInfo: nil)
+        }
     }
     public func exec(sql:String) throws {
         var error:UnsafeMutablePointer<CChar>?
-        sqlite3_exec(self.sqlite, sql, nil, Unmanaged.passUnretained(self).toOpaque(), &error)
+//        sqlite3_exec(self.sqlite, sql, nil, nil, &error)
+
+        sqlite3_exec(self.sqlite, sql, { arg, len, v,col in
+            for i in 0 ..< len{
+                let vstr = v?[Int(i)] == nil ? "NULL" : String(cString: v![Int(i)]!)
+                let cStr = col?[Int(i)] == nil ? "NULL" : String(cString: col![Int(i)]!)
+                print("\(cStr):\(vstr)")
+            }
+            return 0
+        }, nil, &error)
         if let e = error{
             let data = Data(bytes: e, count: strlen(e))
+            sqlite3_free(error)
             throw NSError(domain: String(data: data, encoding: .utf8) ?? "unknow error", code: 0, userInfo: nil)
         }
     }
     public func query(sql:String) throws ->ResultSet{
         var stmt:OpaquePointer?
-        let rc = sqlite3_prepare(self.sqlite, sql, -1, &stmt, nil)
+        let nextSql:UnsafeMutablePointer<UnsafePointer<CChar>?> = .allocate(capacity: 1)
+        let rc = sqlite3_prepare(self.sqlite, sql, Int32(sql.utf8.count), &stmt, nextSql)
         if rc != SQLITE_OK{
-            throw NSError(domain:"query fail" , code: Int(rc), userInfo: nil)
+            throw NSError(domain: String(cString: sqlite3_errmsg(self.sqlite!)), code: Int(rc), userInfo: nil)
         }
-        guard let s = stmt else { throw NSError(domain: "query error", code: 0, userInfo: nil)}
-        return ResultSet(stmt: s)
+        guard let s = stmt else { throw NSError(domain: String(cString: sqlite3_errmsg(self.sqlite!)), code: 0, userInfo: nil)}
+        if let cs = nextSql.pointee{
+            let nsql = String(cString: cs)
+            nextSql.deallocate()
+            return ResultSet(stmt: s, db: self,nextSql: nsql)
+        }else{
+            return ResultSet(stmt: s, db: self)
+        }
+    }
+    public func addScalarFunction(function:ScalarFunction){
+    
+        self.functions.append(function)
+        sqlite3_create_function(self.sqlite!, function.name, function.nArg, SQLITE_UTF8, Unmanaged.passUnretained(function).toOpaque(), { ctx, i, ret in
+            let call = Unmanaged<ScalarFunction>.fromOpaque(sqlite3_user_data(ctx)).takeUnretainedValue()
+            call.call(call,i,ret)
+        }, nil, nil)
     }
     static public func checkCacheDir() throws->URL{
         let name = Bundle.main.bundleIdentifier ?? "main" + ".Database"
-        let url = try FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(name)
+        let url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(name)
         var b:ObjCBool = false
         let a = FileManager.default.fileExists(atPath: url.path, isDirectory: &b)
         if !(b.boolValue && a){
             try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
         }
+        
         return url
+    }
+    public func close(){
+        sqlite3_close(self.sqlite)
     }
     deinit {
         sqlite3_close(self.sqlite)
     }
-    
+    public class ScalarFunction{
+        public let name:String
+        public let nArg:Int32
+        public var ctx:OpaquePointer?
+        public let call:(ScalarFunction,Int32,UnsafeMutablePointer<OpaquePointer?>?)->Void
+        public init(name:String,nArg:Int32,handle:@escaping (ScalarFunction,Int32,UnsafeMutablePointer<OpaquePointer?>?)->Void) {
+            self.name = name
+            self.nArg = nArg
+            self.call = handle
+        }
+        public func ret(v:Int){
+            guard let c = ctx else {
+                return
+            }
+            if MemoryLayout.size(ofValue: v) == 4{
+                sqlite3_result_int(c, Int32(v))
+            }else{
+                sqlite3_result_int64(c, Int64(v))
+            }
+            
+        }
+        public func ret(v:Int32){
+            guard let c = ctx else {
+                return
+            }
+            sqlite3_result_int(c, Int32(v))
+        }
+        public func ret(v:Int64){
+            guard let c = ctx else {
+                return
+            }
+            sqlite3_result_int64(c, Int64(v))
+        }
+        public func ret(v:String){
+            guard let sc = ctx else {
+                return
+            }
+            guard let c = v.cString(using: .utf8) else {
+                sqlite3_result_null(sc)
+                return
+            }
+            let p = UnsafeMutablePointer<CChar>.allocate(capacity: v.utf8.count)
+            memcpy(p, c, v.utf8.count)
+            sqlite3_result_text(sc, v.cString(using: .utf8), Int32(v.utf8.count)) { p in
+                p?.deallocate()
+            }
+        }
+        public func ret(v:Data){
+            guard let sc = ctx else {
+                return
+            }
+            let pointer = sqlite3_malloc(Int32(v.count))
+            let buffer = UnsafeMutableRawBufferPointer(start: pointer, count: v.count)
+            v.copyBytes(to: buffer, count: v.count)
+            sqlite3_result_blob(sc, pointer, Int32(v.count)) { p in
+                sqlite3_free(p)
+            }
+        }
+        public func ret(v:Float){
+            guard let sc = ctx else {
+                return
+            }
+            sqlite3_result_double(sc, Double(v))
+        }
+        public func ret(v:Double){
+            guard let sc = ctx else {
+                return
+            }
+            sqlite3_result_double(sc, v)
+        }
+        public func ret(){
+            guard let sc = ctx else {
+                return
+            }
+            sqlite3_result_null(sc)
+        }
+//        public func h
+    }
     public class ResultSet{
         
         public enum DBDataType{
@@ -68,11 +178,16 @@ public class Database{
             case data
         }
         
-        public var stmt:OpaquePointer
-        public init(stmt:OpaquePointer) {
+        private var stmt:OpaquePointer
+        private var nextSql:String?
+        private unowned var db:Database
+        public init(stmt:OpaquePointer,db:Database,nextSql:String? = nil) {
             self.stmt = stmt
+            self.db = db
+            self.nextSql = nextSql
         }
-        public func next() throws ->Bool{
+        @discardableResult
+        public func step() throws ->Bool{
             let rc = sqlite3_step(self.stmt)
             if rc == SQLITE_ROW{
                 return true
@@ -80,7 +195,23 @@ public class Database{
             if rc == SQLITE_DONE{
                 return false
             }
-            throw NSError(domain: "query error", code: Int(rc), userInfo: nil)
+            throw NSError(domain: String(cString: sqlite3_errmsg(self.db.sqlite!)), code: Int(rc), userInfo: nil)
+        }
+        public func next() throws ->ResultSet?{
+            guard let sql = self.nextSql else {
+                return nil
+            }
+            return try self.db.query(sql: sql)
+        }
+        @discardableResult
+        public func finish()->ResultSet{
+            do {
+                try self.step()
+            } catch {
+                
+            }
+            self.close()
+            return self
         }
         public func close(){
             sqlite3_finalize(self.stmt)
@@ -94,10 +225,14 @@ public class Database{
         public func bind<T>(index:Int32)->Bind<T>{
             Bind<T>.init(stmt: self.stmt, index: index)
         }
-        public func bind<T>(index:Int32,value:T){
-            
+        public func bind<T>(name:String)->Bind<T>?{
+            let index = self.index(paramName: name)
+            if(index == 0){
+                return nil
+            }
+            return Bind<T>.init(stmt: self.stmt, index: index)
         }
-        public func column<T>(index:Int32)->Column<T>{
+        public func column<T>(index:Int32,type:T.Type)->Column<T>{
             Column.init(stmt: self.stmt, index: index)
         }
         
@@ -127,14 +262,20 @@ public class Database{
                 sqlite3_bind_double(self.stmt, index, Double(value))
             }
             public func bind(value:T) where T == String{
-                guard let d = value.data(using: .utf8) else { return }
-                let poiner = UnsafeMutablePointer<CChar>.allocate(capacity: d.count)
-                sqlite3_bind_text(self.stmt, index, poiner, Int32(d.count)) { p in
+                guard let c = value.cString(using: .utf8) else {
+                    sqlite3_bind_null(self.stmt, index)
+                    return
+                }
+                let p = UnsafeMutablePointer<CChar>.allocate(capacity: value.utf8.count)
+                memcpy(p, c, value.utf8.count)
+                sqlite3_bind_text(self.stmt, index, p, Int32(value.utf8.count)) { p in
                     p?.deallocate()
                 }
             }
             public func bind(value:T) where T == Data{
                 let pointer = sqlite3_malloc(Int32(value.count))
+                let buffer = UnsafeMutableRawBufferPointer(start: pointer, count: value.count)
+                value.copyBytes(to: buffer, count: value.count)
                 sqlite3_bind_blob64(self.stmt, index, pointer, sqlite3_uint64(value.count)) { po in
                     sqlite3_free(po)
                 }
@@ -178,6 +319,12 @@ public class Database{
                 guard let byte = sqlite3_column_blob(self.stmt, index) else { return Data() }
                 return Data(bytes: byte, count: Int(len))
             }
+            public var tableName:String{
+                String(cString: sqlite3_column_table_name(self.stmt, index))
+            }
+            public var databaseName:String{
+                String(cString: sqlite3_column_database_name(self.stmt, index))
+            }
             public var name:String{
                 String(cString: sqlite3_column_name(self.stmt, index))
             }
@@ -196,7 +343,6 @@ public class Database{
                     return .Null
                 }
             }
-            
         }
     }
     
--- WebImageTests/WebImageTests.swift
+++ WebImageTests/WebImageTests.swift
@@ -7,40 +7,115 @@
 
 import XCTest
 @testable import WebImageCache
+import SQLite3
 class WebImageTests: XCTestCase {
     
-    var web:Downloader = Downloader(configuration: .default)
 
     var data:Database?
     override func setUpWithError() throws {
-        self.data = try Database(group: DispatchGroup(), queue: .main, name: "a")
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
+//        self.data = nil
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testCreateDropTable() throws {
-        
+        self.data = try Database(group: DispatchGroup(), queue: .main, name: "a")
         try self.data?.exec(sql: "CREATE TABLE m (Field1    INTEGER NOT NULL UNIQUE,Field2    INTEGER,PRIMARY KEY(Field1,Field2))")
         
         try self.data?.exec(sql: "DROP table m")
+        self.data?.close()
     }
     func testSimpleInsert() throws {
         
-        
+        self.data = try Database(group: DispatchGroup(), queue: .main, name: "a")
         try self.data?.exec(sql: "CREATE TABLE IF NOT EXISTS A (Field1    INTEGER,Field2    TEXT,Field3    REAL,Field4    NUMERIC,Field5    BLOB)")
         let result = try self.data?.query(sql: "INSERT INTO A  VALUES (?,?,?,?,?)")
-        result?.writeParam(index: 1, param: Int32(1))
-        result?.writeParam(index: 2, param: "2")
-        result?.writeParam(index: 3, param: Double(3.14))
-        result?.writeParam(index: 4, param: Double(1.414))
-        result?.writeParam(index: 5, param: "12312313".data(using: .utf8)!)
-        try result?.next()
-        result?.close()
-        print(self.data?.url)
+        result?.bind(index: 1).bind(value: 1)
+        result?.bind(index: 2).bind(value: "ds撒打算😗👨‍👨‍👧‍👦ds")
+        result?.bind(index: 3).bind(value: 3.14)
+        result?.bind(index: 4).bind(value: 1.414)
+        result?.bind(index: 5).bind(value: "dsdsd".data(using: .utf8)!)
+        result?.finish()
+        self.data?.close()
+    }
+    func testQuery() throws{
+        self.data = try Database(group: DispatchGroup(), queue: .main, name: "a")
+        let result = try self.data!.query(sql: "SELECT * from A where Field1=:name")
+        result.bind(name: ":name")?.bind(value: 1)
+        while try result.step() {
+            print(result.column(index: 0, type: Int.self).value())
+            print(result.column(index: 1, type: String.self).value())
+            print(result.column(index: 2, type: Float.self).value())
+            print(result.column(index: 3, type: Float.self).value())
+            print(result.column(index: 4, type: String.self).value())
+        }
+        result.close()
+        self.data?.close()
+    }
+    func testTransactions() throws{
+        let sql2 = """
+        BEGIN;
+                        CREATE TABLE IF NOT EXISTS emp_master
+
+                        (emp_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+                        first_name TEXT,
+
+                        last_name TEXT,
+
+                        salary NUMERIC,
+
+                        dept_id INTEGER);
+
+        INSERT INTO emp_master
+
+        values (1,'Honey','Patel', 10100,1),
+
+                (2,'Shweta','Jariwala', 19300,2),
+
+                (3,'Vinay','Jariwala', 35100,3),
+
+                (4,'Jagruti','Viras', 9500,2),
+
+                (5,'Shweta','Rana',12000,3),
+
+                (6,'sonal','Menpara', 13000,1),
+
+                (7,'Yamini','Patel', 10000,2),
+
+                (8,'Khyati','Shah', 50000,3),
+
+                (9,'Shwets','Jariwala',19400,2);
         
+                INSERT INTO emp_master VALUES (10,'yinhao','Francis',99999,1);
+
+        COMMIT;
+        """
+        self.data = try Database(group: DispatchGroup(), queue: .main, name: "a")
+//        try self.data!.query(sql: sql).finish(
+        try self.data?.exec(sql: sql2)
+//        DELETE FROM emp_master WHERE emp_id=1;
+//        try self.data?.query(sql: sql2).finish()
+        self.data?.close()
+        print(data?.url)
+    }
+    func testFunc() throws {
+        let sql2 = """
+        select emp_id,A(emp_id) from emp_master;
+        """
+        self.data = try Database(group: DispatchGroup(), queue: .main, name: "a")
+        let f = Database.ScalarFunction(name: "A", nArg: 1) {ctx, i, a in
+            sqlite3_result_int(ctx, sqlite3_value_int(a?.pointee) + 1)
+            
+        }
+        self.data?.addScalarFunction(function: f)
+//        self.data?.addScalarFunction(function: Function)
+        var result = try self.data?.exec(sql: sql2)
+        self.data?.close()
     }
 
     func testPerformanceExample() throws {
@@ -51,3 +126,4 @@ class WebImageTests: XCTestCase {
     }
 
 }
+        
