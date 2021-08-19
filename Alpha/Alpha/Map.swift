0f211628f06b0bc4abe793f00d4a99809c66b7c7
yinhao <yinhao@5eplay.com>
2021-08-19 10:21:25 +0800

    heap

--- Alpha/Alpha/Map.swift
+++ Alpha/Alpha/Map.swift
@@ -94,19 +94,31 @@ public class List<K:Hashable>{
         return c
     }
 }
-public class BTree<T>:CustomStringConvertible{
+public class BTree<T:Equatable>:CustomStringConvertible{
+    public func lock(call:()->Void){
+        pthread_mutex_lock(self.lock)
+        call()
+        pthread_mutex_unlock(self.lock)
+    }
+    public func lockAccess(call:()->T)->T{
+        pthread_mutex_lock(self.lock)
+        let r = call()
+        pthread_mutex_unlock(self.lock)
+        return r
+        
+    }
     public var description: String{
         return self.array.description
     }
 
     private var array:Array<T> = Array()
-    private var rw:UnsafeMutablePointer<pthread_rwlock_t> = .allocate(capacity: 1)
+    public private(set) var lock:UnsafeMutablePointer<pthread_mutex_t> = .allocate(capacity: 1)
     public init() {
-        pthread_rwlock_init(self.rw, nil)
+        pthread_mutex_init(self.lock, nil)
     }
     deinit {
-        pthread_rwlock_destroy(self.rw)
-        rw.deallocate()
+        pthread_mutex_destroy(self.lock)
+        self.lock.deallocate()
     }
     public struct Node:Equatable{
         var index:Int
@@ -123,39 +135,41 @@ public class BTree<T>:CustomStringConvertible{
     }
     public subscript(index:Node)->T{
         get{
-            pthread_rwlock_rdlock(self.rw)
             let temp = array[index.index]
-            pthread_rwlock_unlock(self.rw)
             return temp
         }
         set{
-            pthread_rwlock_wrlock(self.rw)
             self.array[index.index] = newValue
-            pthread_rwlock_unlock(self.rw)
         }
     }
     public func append(object:T){
-        pthread_rwlock_wrlock(self.rw)
         self.array.append(object)
-        pthread_rwlock_unlock(self.rw)
     }
     public func removelast(){
-        pthread_rwlock_wrlock(self.rw)
         if array.count > 0{
             array.removeLast()
         }
-        pthread_rwlock_unlock(self.rw)
+
+    }
+    public func index(content:T)->Node?{
+        guard let index = self.array.firstIndex(of: content) else { return nil }
+        return Node(index: index)
+    }
+    public func remove(node:Node){
+     
+        self.array.remove(at: node.index)
+      
     }
     public func isEmpty(index:Node)->Bool{
-        pthread_rwlock_rdlock(self.rw)
+ 
         let temp = index.index >= self.array.count
-        pthread_rwlock_unlock(self.rw)
+  
         return temp
     }
     public func isLeaf(index:Node)->Bool{
-        pthread_rwlock_rdlock(self.rw)
+      
         let temp = self.isEmpty(index: index.leftNode) && self.isEmpty(index: index.rightNode)
-        pthread_rwlock_unlock(self.rw)
+     
         return temp
     }
     public var lastNode:Node{
@@ -163,6 +177,16 @@ public class BTree<T>:CustomStringConvertible{
     }
     public var firstNode:Node{
         Node(index: 0)
+    }
+    public func removeLastLeaf(count:Int)->ArraySlice<T>{
+    
+        if array.count == 0 || count > array.count{
+            return []
+        }
+        let r = array[(array.count - count) ..< array.count]
+        array.removeSubrange((array.count - count) ..< array.count)
+        
+        return r
     }
     public var count:Int{
         return self.array.count
@@ -174,39 +198,68 @@ public class Heap<T:Comparable>:BTree<T>{
         self.compare = compare
     }
     public func insert(object:T){
-        self.append(object: object)
-        var current = self.lastNode
-        var parent = current.parent
-        if(current != self.firstNode){
-            var parentV = self[parent]
-            while compare(parentV,object) && current != self.firstNode{
-                
-                self[parent] = self[current]
-                self[current] = parentV
-                current = parent
-                parent = current.parent
-                if parent.index < 0{
-                    break
+        self.lock{
+            self.append(object: object)
+            var current = self.lastNode
+            var parent = current.parent
+            if(current != self.firstNode){
+                var parentV = self[parent]
+                while compare(parentV,object) && current != self.firstNode{
+                    
+                    self[parent] = self[current]
+                    self[current] = parentV
+                    current = parent
+                    parent = current.parent
+                    if parent.index < 0 || self.count == 0{
+                        break
+                    }
+                    parentV = self[parent]
                 }
-                parentV = self[parent]
             }
         }
     }
-    public func remove()->T{
-        let result = self[self.firstNode]
-        let last = self[self.lastNode]
-        self.removelast()
-        if(self.count == 0){
-            return result
-        }
-        self[firstNode] = last
-        var current = self.firstNode
-        var l = current.leftNode
-        var r = current.rightNode
-        
-        while !self.isEmpty(index: l) || !self.isEmpty(index: r) {
-            if !self.isEmpty(index: l) && !self.isEmpty(index: r){
-                if self.compare(self[r],self[l]){
+    public func remove()->T {
+        self.remove(node: self.firstNode)
+    }
+    public func remove(node:Node)->T{
+        return self.lockAccess {
+            let result = self[node]
+            let last = self[self.lastNode]
+            self.removelast()
+            if(self.count == 0){
+                return result
+            }
+            self[node] = last
+            var current = node
+            var l = current.leftNode
+            var r = current.rightNode
+            
+            while !self.isEmpty(index: l) || !self.isEmpty(index: r) {
+                if !self.isEmpty(index: l) && !self.isEmpty(index: r){
+                    if self.compare(self[r],self[l]){
+                        if self.compare(self[current],self[l]){
+                            let pv = self[current]
+                            let lv = self[l]
+                            self[l] = pv
+                            self[current] = lv
+                            current = l
+                            l = current.leftNode
+                            r = current.rightNode
+                            continue
+                        }
+                    }else{
+                        if self.compare(self[current],self[r]){
+                            let pv = self[current]
+                            let rv = self[r]
+                            self[r] = pv
+                            self[current] = rv
+                            current = r
+                            l = current.leftNode
+                            r = current.rightNode
+                            continue
+                        }
+                    }
+                }else if !self.isEmpty(index: l){
                     if self.compare(self[current],self[l]){
                         let pv = self[current]
                         let lv = self[l]
@@ -217,7 +270,7 @@ public class Heap<T:Comparable>:BTree<T>{
                         r = current.rightNode
                         continue
                     }
-                }else{
+                }else if !self.isEmpty(index: r){
                     if self.compare(self[current],self[r]){
                         let pv = self[current]
                         let rv = self[r]
@@ -229,33 +282,11 @@ public class Heap<T:Comparable>:BTree<T>{
                         continue
                     }
                 }
-            }else if !self.isEmpty(index: l){
-                if self.compare(self[current],self[l]){
-                    let pv = self[current]
-                    let lv = self[l]
-                    self[l] = pv
-                    self[current] = lv
-                    current = l
-                    l = current.leftNode
-                    r = current.rightNode
-                    continue
-                }
-            }else if !self.isEmpty(index: r){
-                if self.compare(self[current],self[r]){
-                    let pv = self[current]
-                    let rv = self[r]
-                    self[r] = pv
-                    self[current] = rv
-                    current = r
-                    l = current.leftNode
-                    r = current.rightNode
-                    continue
-                }
+                break
             }
-            break
+            
+            return result
         }
-        
-        return result
     }
 }
 public class MaxHeap<T:Comparable>:Heap<T>{
@@ -266,5 +297,64 @@ public class MaxHeap<T:Comparable>:Heap<T>{
 public class MinHeap<T:Comparable>:Heap<T>{
     public init(){
         super.init(compare: {$0>$1})
+    }
+}
+public class Priority<T:Equatable>:Comparable{
+    public static func == (lhs: Priority<T>, rhs: Priority<T>) -> Bool {
+        lhs.count == rhs.count
+    }
+    public init(content:T,name:String) {
+        self.content = content
+        self.name = name
+    }
+    public var content:T
+    public var count:Int = 1
+    public var date:Date = Date()
+    public var name:String
+    public static func < (lhs: Priority<T>, rhs: Priority<T>) -> Bool {
+        if(rhs.count > lhs.count){
+            return true
+        }else if(rhs.count == lhs.count){
+            if(rhs.date > lhs.date){
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+    }
+}
+
+public class Cache<T:Equatable>{
+    var minHeap:MinHeap<Priority<T>> = MinHeap()
+    var map:Map<String,Priority<T>> = Map()
+    var queue = DispatchQueue.global()
+    public var maxCount:Int = 50000
+    public func setContent(key:String,content:T){
+        if self.contain(key: key){
+            let p = self.map[key]
+            p?.content = content
+        }else{
+            let p = Priority(content: content, name: key)
+            self.map[key] = p
+            self.minHeap.insert(object: p)
+            self.queue.async {
+                if self.maxCount < self.minHeap.count{
+                    let a = self.minHeap.remove()
+                    self.map[a.name] = nil
+                }
+            }
+        }
+        
+    }
+    public func contain(key:String)->Bool{
+        self.map[key] != nil
+    }
+    public func content(key:String)->T?{
+        let p = map[key]
+        p?.count += 1
+        p?.date = Date()
+        return p?.content
     }
 }
--- Alpha/AlphaTests/HeapTest.swift
+++ Alpha/AlphaTests/HeapTest.swift
@@ -55,4 +55,66 @@ class HeapTest: XCTestCase {
             XCTAssert(array[i] <= array[i + 1])
         }
     }
+    func testRemoveHeap() throws {
+        let a = MinHeap<UInt32>()
+        
+   
+        for _ in 0..<10 {
+            a.insert(object: arc4random() % 100)
+            print(a)
+        }
+        for _ in 0..<10{
+            a.remove(node: a.firstNode)
+            print(a)
+        }
+    }
+    func testCache() throws{
+        let cache = Cache<Int>()
+
+        let group = DispatchGroup()
+        for i in 0 ..< 100{
+            group.enter()
+            DispatchQueue.global().async {
+                cache.setContent(key: "\(i)", content: i)
+                group.leave()
+            }
+            
+        }
+        for i in 0 ..< 100000 {
+            group.enter()
+            DispatchQueue.global().async {
+                let a = cache.content(key: "\(i % 100)")
+                XCTAssert(a == i % 100)
+                group.leave()
+            }
+        }
+        let a = XCTestExpectation(description: "time out")
+        group.notify(queue: .main) {
+            a.fulfill()
+            
+            print("finish")
+        }
+        self.wait(for: [a], timeout: 1000)
+    }
+    func testWrite(){
+        
+        measure {
+            let cache = Cache<Data>()
+            for i in 0 ..< 1000000{
+                cache.setContent(key: "\(i % 60000)", content: "dasdasdad".data(using: .utf8)!)
+            }
+        }
+    }
+    func testRead(){
+        let cache = Cache<Data>()
+        for i in 0 ..< cache.maxCount {
+            cache.setContent(key: "\(i)", content: "dasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgkdasdadadadadfajhsgdjhfasgdfjaksgdfajksfgaksjhdfgajskdfgajsdfgaskjdfgk".data(using: .utf8)!)
+        }
+        measure {
+            for i in 0 ..< 1000000{
+                cache.content(key: "\(arc4random() % 70000)")
+            }
+        }
+    }
+    
 }
